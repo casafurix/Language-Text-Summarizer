20bd172dbe9dfae55d492db6983a2b2290fdb6d3
casafurix <agnibha10@gmail.com>
2022-08-03 19:16:07 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Language Text Summarizer
+
+An AI/ML based NLP solution that generates short summaries of press releases/news articles.
+
+## Model description
+
+BART is a transformer encoder-encoder (seq2seq) model with a bidirectional (BERT-like) encoder and an autoregressive (GPT-like) decoder. BART is pre-trained by (1) corrupting text with an arbitrary noising function, and (2) learning a model to reconstruct the original text.
+
+BART is particularly effective when fine-tuned for text generation (e.g. summarization, translation) but also works well for comprehension tasks (e.g. text classification, question answering). This particular checkpoint has been fine-tuned on CNN Daily Mail, a large collection of text-summary pairs
---
+++ app.py
@@ -0,0 +1,57 @@
+# backend -> frontend, frontend -> backend (jinja from Flask)
+
+from googletrans import Translator
+from flask import Flask, render_template
+import requests
+from flask import request as req
+
+
+app = Flask(__name__)
+
+translator = Translator()
+
+
+@app.route("/", methods=["GET", "POST"])
+def Index():
+    return render_template("index.html")
+
+
+@app.route("/Summarise", methods=["GET", "POST"])
+def Summarise():
+    if req.method == "POST":
+        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+        headers = {"Authorization": f"Bearer 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"}
+
+        data = req.form["data"]
+
+        # translating hindi input from user into english
+        translated_input = translator.translate(data, dest="en")
+
+        maxL = int(req.form["maxL"])
+        minL = maxL // 4
+
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+
+        output = query(
+            {
+                "inputs": translated_input.text,
+                "parameters": {
+                    "min_length": minL,
+                    "max_length": maxL,
+                },
+            }
+        )[0]
+
+        # translating english output from backend to hindi output for user
+        translated_output = translator.translate(output["summary_text"], dest="hi")
+
+        return render_template("index.html", result=translated_output.text)
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
